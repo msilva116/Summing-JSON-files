133df0afe1742da30e79087c6e0e6a62910f497b
Robert Fisher <robert.fisher@umassd.edu>
2018-01-23 16:26:51 -0500

    Added code to compute mean abundances and output these values in reverse order.

--- particle_parsing.py
+++ particle_parsing.py
@@ -1,10 +1,38 @@
-#Prints data from python: adds a u before each isotope (u'ir191': 0.0,)
+#======================================================================
+#
+#  particle_parsing.py - A short script to read particle data in in
+#   JSON format. The script also loops over all particle and abundances
+#   to compute the mean abundances, stored in the dictionary mean_abundance.
+#   The mean_abundance dictionary is output in reverse order.
+#
+#    - RTF, 1/23/18
+#
+#======================================================================
+
 import json
-from pprint import pprint
 
-data = json.load(open('data.JSON'))
-particle = data[99]
-print particle ['c12']
-#pprint(data)
+# Read particle data in as JSON format
+particle_list = json.load(open('data.JSON'))
 
-#Found above example at https://stackoverflow.com/questions/2835559/parsing-values-from-a-json-file. Tried to parse out values using Justin Peel's example, but couldn't determine what values in the example file corruspond with the values in the actual, data.JSON  file. 
+# Initialize empty dictionary for mean abundances
+
+mean_abundance = {}
+
+for isotope in particle_list [0]:
+  mean_abundance [isotope] = 0.0
+
+# Loop over all particles, sum abundances
+
+for particle in particle_list:
+  for isotope in particle:
+    mean_abundance [isotope] = mean_abundance [isotope] + particle [isotope]
+
+# Calculate mean abundance by normalizing to number of particles
+
+for isotope in mean_abundance:
+  mean_abundance [isotope] = mean_abundance [isotope] / len (particle_list)
+
+# Use a lambda function to print out the dictionary of mean abundances,
+# sorted by value, in reverse order, e.g. from largest to smallest
+
+print sorted(mean_abundance.items(), key=lambda x:x[1], reverse=True )
